f1def9753ff6920d2bc6af46cb1bb3b0fbbb99c1
Hector Sanjuan <hsanjuan@visteon.com>
2020-02-12 12:55:55 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace tablesSoC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.TestNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TestSteps = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnExportOpen = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TestNumber,
+            this.TestSteps});
+            this.dataGridView1.Location = new System.Drawing.Point(63, 221);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(493, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // TestNumber
+            // 
+            this.TestNumber.HeaderText = "Test Number";
+            this.TestNumber.MinimumWidth = 6;
+            this.TestNumber.Name = "TestNumber";
+            this.TestNumber.Width = 125;
+            // 
+            // TestSteps
+            // 
+            this.TestSteps.HeaderText = "Test Steps";
+            this.TestSteps.MinimumWidth = 6;
+            this.TestSteps.Name = "TestSteps";
+            this.TestSteps.Width = 125;
+            // 
+            // btnExportOpen
+            // 
+            this.btnExportOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportOpen.Location = new System.Drawing.Point(63, 410);
+            this.btnExportOpen.Name = "btnExportOpen";
+            this.btnExportOpen.Size = new System.Drawing.Size(136, 57);
+            this.btnExportOpen.TabIndex = 8;
+            this.btnExportOpen.Text = "Export/Open Excel";
+            this.btnExportOpen.UseVisualStyleBackColor = true;
+            this.btnExportOpen.Click += new System.EventHandler(this.btnExportOpen_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "*.xlsx | *.xlsx";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(63, 27);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(493, 153);
+            this.dataGridView2.TabIndex = 9;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(tablesSoC.dispGp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 498);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnExportOpen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TestNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TestSteps;
+        private System.Windows.Forms.Button btnExportOpen;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Office.Interop.Excel;
+using DataTable = System.Data.DataTable;
+
+namespace tablesSoC
+{
+    public partial class Form1 : Form
+    {
+        string connString = @"Data Source=MXD64L17S2\SQLEXPRESS;Initial Catalog=displayGraphics;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlDataAdapter dataAdapter;
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExportOpen_Click(object sender, EventArgs e)
+        {
+            _Application excel = new Microsoft.Office.Interop.Excel.Application();
+            _Workbook workbook = excel.Workbooks.Add(Type.Missing);
+            _Worksheet worksheet = null;
+
+            try
+            {
+                worksheet = workbook.ActiveSheet;
+                worksheet.Name = "Test Cases";
+
+                for (int rowIndex = 0; rowIndex < dataGridView1.Rows.Count; rowIndex++)
+                {
+                    for (int colIndex = 0; colIndex < dataGridView1.Columns.Count; colIndex++)
+                    {
+                        worksheet.Cells[rowIndex + 1, colIndex + 1] =
+                            dataGridView1.Rows[rowIndex].Cells[colIndex].Value.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                excel.Quit();
+                workbook = null;
+                excel = null;
+            }
+
+            //if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    workbook.SaveAs(saveFileDialog1.FileName);
+            //    Process.Start("excel.exe", saveFileDialog1.FileName);
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = bindingSource1;
+            GetData("Select * from dispGp");
+
+            string[] ignSt = { "0x4 (Run)", "0x8 (Start)" };
+            string NotRdytoDrvMde = null;
+            string SoCDisplayed = null;
+            string SoCInvalid = null;
+            string SoCFillColor = null;
+            string AmbientTheme = null;
+            string Litval = null;
+            string HwyAssistActv = null;
+            string final = null;
+
+            for (int rowIndex = 0; rowIndex <= 41; rowIndex++) //leer la base de datos
+            {
+                for (int colIndex = 1; colIndex < dataGridView2.Columns.Count; colIndex++) //colIndex = 1 ignore ID field
+                {
+                    switch (colIndex) //convertir a señales CAN
+                    {
+                        case 1:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "No")
+                                NotRdytoDrvMde = "0x1 (On)";
+                            else
+                                NotRdytoDrvMde = "0x0 (Off)";
+                            break;
+                        case 2:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() != "Not Displayed")
+                                SoCDisplayed = "0x0 (SelDrvMde01)";
+                            else
+                                SoCDisplayed = "0x1 (SelDrvMde02)";
+                            break;
+                        case 3:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "Yes")
+                                SoCInvalid = "0xFF (Invalid)";
+                            break;
+                        case 4:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "100")
+                                SoCInvalid = "0x7F (100%)";
+                            else if (dataGridView2[colIndex, rowIndex].Value.ToString() == "99")
+                                SoCInvalid = "0x3F (10 to 99)";
+                            else if (dataGridView2[colIndex, rowIndex].Value.ToString() == "9")
+                                SoCInvalid = "0xF (0 to 9)";
+                            break;
+                        case 5:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "Blue")
+                                SoCFillColor = "0x0 (Null)";
+                            else if (dataGridView2[colIndex, rowIndex].Value.ToString() == "Amber")
+                                SoCFillColor = "0x1 (LoDTE)";
+                            else
+                                SoCFillColor = "0x2 (ZeroDteDepletedBattery)";
+                            break;
+                        case 6:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "Light")
+                            {
+                                AmbientTheme = "0x1 (Auto Day)";
+                                Litval = "0x0 (Night)";
+                            }
+                            else if (dataGridView2[colIndex, rowIndex].Value.ToString() == "Dark Night")
+                            {
+                                AmbientTheme = "0x2 (Auto Night)";
+                                Litval = "0x3 (Twilight)";
+                            }
+                            else
+                            {
+                                AmbientTheme = "0x4 (Manual Night Bright)";
+                                Litval = "0xFF (Invalid)";
+                            }
+                            break;
+                        case 7:
+                            if (dataGridView2[colIndex, rowIndex].Value.ToString() == "No")
+                                HwyAssistActv = "0";
+                            break;
+                    }
+                }
+
+                for (int k = 0; k < ignSt.Length; k++) //repetir el test case por cada power mode
+                {
+                    final += "*)" + " " + "Set VOPS Configuration" + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "Ignition_Status = " + ignSt[k] + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "PwPckTqRdy_B_Dsply = " + NotRdytoDrvMde + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "PwPckTqRdy_B_Dsply_UB = 0x1 (Fresh)" + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "ActvDrvMde_D2_Stat = " + SoCDisplayed + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "BattTracSoc_Pc_Dsply = " + SoCInvalid + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "BattTracSoc_Pc_Dsply_UB = 0x1 (Fresh)" + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "BattTracLoSoc_D_Dsply = " + SoCFillColor + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "DrvDsplyPalette_D_Stat = " + AmbientTheme + "," + "\n";
+                    final += "*)" + " " + "Send signal periodically: " + "Litval = " + Litval + "," + "\n";
+                    final += "*)" + " " + "Populate results";
+                }
+
+                // this part up to the end could be outside of both for loops
+                var countSt = final.Split(new char[] { '*' });
+                int lines = countSt.Length / ignSt.Length; //it is repeated per power mode
+
+                for (int i = 1; i < countSt.Length; i++)
+                {
+                    int m = (i - 1) / lines;
+                    int n = i - (lines * m);
+                    countSt[i] = $"{n}" + countSt[i];
+
+                    if ((n - 1) % lines == 0)
+                        dataGridView1.Rows.Add(); //create a new row in the datagrid every lines lines
+                }
+
+                for (int j = dataGridView1.Rows.Count - 3; j < dataGridView1.Rows.Count - 1; j++) //must play with row numbers
+                { 
+                    if (j % 2 == 0) //separate the string array in two, maybe I should have used substring or slice
+                    {
+                        for (int i = 1; i < (countSt.Length / 2) + 1; i++)
+                        {
+                            dataGridView1[1, j].Value += countSt[i];
+                            dataGridView1[0, j].Value = j;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = (countSt.Length / 2) + 1; i < countSt.Length; i++)
+                        {
+                            dataGridView1[1, j].Value += countSt[i];
+                            dataGridView1[0, j].Value = j;
+                        }
+                    }
+                }
+                final = ""; // this is not necessary, the end 
+            }
+        }
+
+        private void GetData(string selectCommand)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter(selectCommand, connString);
+                table = new DataTable();
+                table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+                dataAdapter.Fill(table);
+                bindingSource1.DataSource = table;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
