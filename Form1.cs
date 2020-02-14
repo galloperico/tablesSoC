accf0073486bbf01eb8bfa6903df8ab94f20e376
Hector Sanjuan <hsanjuan@visteon.com>
2020-02-14 06:41:51 -0600

    no vops

--- Form1.cs
+++ Form1.cs
@@ -110,13 +110,13 @@ namespace tablesSoC
 
                 for (int k = 0; k < ignSt.Length; k++) //repetir el test case por cada power mode
                 {
-                    final += "*)" + " " + "Set VOPS Configuration" + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "Ignition_Status = " + ignSt[k] + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "PwPckTqRdy_B_Dsply = " + NotRdytoDrvMde + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "PwPckTqRdy_B_Dsply_UB = 0x1 (Fresh)" + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "ActvDrvMde_D2_Stat = " + SoCDisplayed + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "DrvDsplyPalette_D_Stat = " + AmbientTheme + "," + "\n";
                     final += "*)" + " " + "Send signal periodically: " + "Litval = " + Litval + "," + "\n";
+                    final += "*)" + " " + "Get screenshot and verify if display graphic is present: " + "," + "\n";
                     final += "*)" + " " + "Populate results";
                 }
 
